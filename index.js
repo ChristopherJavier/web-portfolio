060c879eda95904fdc9472ce055c0102b7bbfce0
Christopher Javier <christopherjavier512@gmail.com>
2023-05-22 19:28:02 -0400

    Added some new components and restructured the HTML, CSS, and new functions in index.js

--- index.js
+++ index.js
@@ -1,59 +1,112 @@
-let projectsCont = document.getElementById('projects_cont');
+let projectsCont = document.getElementById("projects_cont");
+let techsImgs = document.getElementById("techs_imgs");
+const techListElement = document.getElementById("techList");
 
 function getData() {
-    fetch('./db.json')
-        .then(res => res.json())
-        .then(data => formatData(data.projects));
+  fetch("./db.json")
+    .then((res) => res.json())
+    .then((data) => {
+      formatData(data.projects);
+      getTechs(data.techs);
+    });
 }
 
 function formatData(data) {
-    let formattedData = []
-    const names = data.name;
-    const descriptions = data.description;
-    const technologies = data.technologies;
-    const github = data.github;
-    const links = data.links;
+  let formattedData = [];
+  const names = data.name;
+  const descriptions = data.description;
+  const technologies = data.technologies;
+  const github = data.github;
+  const links = data.links;
 
-    for (let i = 0; i < names.length; i++) {
-        formattedData = formattedData.concat({
-            name: names[i],
-            descriptions: descriptions[i],
-            technologies: technologies[i],
-            github: github[i],
-            links: links[i]
-        })
-    }
+  for (let i = 0; i < names.length; i++) {
+    formattedData = formattedData.concat({
+      name: names[i],
+      descriptions: descriptions[i],
+      technologies: technologies[i],
+      github: github[i],
+      links: links[i],
+    });
+  }
 
-    showData(formattedData)
+  showData(formattedData);
 }
 
 function showData(data) {
-    console.log(data)
-    data.map(function (project) {
-        let div = document.createElement('div')
-        let name = document.createElement('h3')
-        let description = document.createElement('p')
-        let ul = document.createElement('ul')
-        let li = document.createElement('li')
-        name.innerHTML = `${project.name}`
-        description.innerHTML = `${project.descriptions}`
-        ul.className = "tech_list"
-        div.className = "project-box"
+  data.map(function (project) {
+    let div = document.createElement("div");
+    let name = document.createElement("h3");
+    let description = document.createElement("p");
+    let ul = document.createElement("ul");
+    let li = document.createElement("li");
+    name.innerHTML = `${project.name}`;
+    description.innerHTML = `${project.descriptions}`;
+    ul.className = "tech_list";
+    div.className = "project-box";
 
-        project.technologies.map(function (i) {
-            let img = document.createElement('img')
-            img.className = "tech_icons_img"
-            li.className = "tech_icons"
-            img.src = `./assets/${i}.png`
-            li.appendChild(img)
-            ul.append(li)
-        })
+    project.technologies.map(function (i) {
+      let img = document.createElement("img");
+      img.className = "tech_icons_img";
+      li.className = "tech_icons";
+      img.src = `./assets/${i}.png`;
+      li.appendChild(img);
+      ul.append(li);
+    });
 
-        div.appendChild(name)
-        div.appendChild(description)
-        div.appendChild(ul)
-        projectsCont.appendChild(div)
-    })
+    div.appendChild(name);
+    div.appendChild(description);
+    div.appendChild(ul);
+    projectsCont.appendChild(div);
+  });
+}
+
+function getTechs(data) {
+  for (const k in data) {
+    const techCategory = document.createElement("div");
+    const techCategoryTitle = document.createElement("h3");
+    techCategory.className = "techCategory";
+    techCategoryTitle.innerHTML = k;
+    techCategory.append(techCategoryTitle);
+
+    data[k].map((i) => {
+      const techCard = document.createElement("div");
+      const techImg = document.createElement("img");
+      const techName = document.createElement("p");
+      techCard.className = "techCard";
+
+      techName.innerHTML = i;
+      techImg.src = `./assets/${i}.png`;
+      techImg.id = `${k.replace(/\s/g, "")}T${data[k].indexOf(i)}`
+
+      techCard.append(techImg);
+      techCard.append(techName);
+
+      techCategory.append(techCard);
+    });
+
+    techListElement.append(techCategory);
+  }
+}
+
+async function getLinks() {
+  let links;
+
+  await fetch("./db.json")
+    .then((res) => res.json())
+    .then((data) => (links = data.user));
+
+  for (const key in links) {
+    if (key === "profile_photo") {
+      document.getElementById(key).src = links[key];
+    }
+
+    for (const k in document.getElementsByClassName(`${key}_link`)) {
+      if (!isNaN(parseInt(k))) {
+        document.getElementsByClassName(`${key}_link`)[k].href = links[key];
+      }
+    }
+  }
 }
 
 getData();
+getLinks();
